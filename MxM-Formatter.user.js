133cb5f9542df5ff1eff1af4cd541c1d807c8277
AshtonLG3 <ngeziman@live.com>
2025-10-16 14:14:40 +0200

    Create MxM-Formatter.user.js

---
+++ MxM-Formatter.user.js
@@ -0,0 +1,2 @@
+// @downloadURL  https://raw.githubusercontent.com/AshtonLG3/Musixmatch-In-Editor-Lyrics-Formatter/main/MxM-Formatter.user.js
+// @updateURL    https://raw.githubusercontent.com/AshtonLG3/Musixmatch-In-Editor-Lyrics-Formatter/main/MxM-Formatter.user.js
